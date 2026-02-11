27ecd752c9892a5b63227f1034819f33ffda422c
User <user@example.com>
2026-02-11 16:18:51 -0500

    Prepare for Vercel deployment

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import path from 'path';
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig(({ mode }) => {
+    const env = loadEnv(mode, process.cwd(), '');
+    return {
+      server: {
+        port: 3000,
+        host: '0.0.0.0',
+      },
+      plugins: [react()],
+      resolve: {
+        alias: {
+          '@': path.resolve(__dirname, './src'),
+        }
+      },
+      // Vercel deployment often requires specific build settings
+      build: {
+        outDir: 'dist',
+        sourcemap: false
+      }
+    };
+});
